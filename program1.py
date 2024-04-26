fe65c8333cab0e8632bba0f2ba58bd2c4c91183a
ishanparwar0012 <168174422+ishanparwar0012@users.noreply.github.com>
2024-04-26 14:03:15 +0530

    Ishan Parwar

--- program1.py
+++ program1.py
@@ -1,4 +1,4 @@
-def smallest_missing_positive_integer(nums: List[int]) -> int:
+def smallest_missing_positive_integer(nums):
     """
     Implement the function smallest_missing_positive_integer 
     using the provided smallest_missing_positive_integer function 
@@ -8,14 +8,20 @@ def smallest_missing_positive_integer(nums: List[int]) -> int:
     pass
 
 
+    from typing import List
 
+    n = len(nums)
 
+    for i in range(n):
+        while 1 <= nums[i] <= n and nums[nums[i] - 1]!= nums[i]:
+            correct_index = nums[i] - 1
+            nums[i], nums[correct_index] = nums[correct_index], nums[i]
 
+    for i in range(n):
+        if nums[i]!= i + 1:
+            return i + 1
 
+    return n + 1
 
-    
-
-
-
-  
-
+    print(smallest_missing_positive_integer)
+pass
